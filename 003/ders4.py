adcbc29985edb6a8597f0ab87873dee4b88beca4
nurrbb <enurb320@gmail.com>
2026-01-30 21:40:59 +0300

    feat: implement practice scripts for conditional logic and user inputs

---
+++ 003/ders4.py
@@ -0,0 +1,76 @@
+# print("Nur" == "nur") #eşit midir
+# print(10/2 == 5)
+# print(7 % 2 == 1)
+# print(10 % 2 == 0)
+
+#ctrl+ö ile yorum satırına alalım
+
+# print(100 // 3 == 33) 
+# print(2**3 > 10)
+
+# yas = 18
+# print( yas >= 18 )
+
+# isim = "Can"
+# print( isim != "Ali") # farklı mı?  evet -> true ---- hayır -> false
+
+# bakiye = 100
+# harcama = 120
+
+# print(bakiye >= harcama)
+
+# print(5.0 == 5)
+
+# print(0 == False) # bilgisayar dilinde 
+# print(1 == True )
+
+#ctrl+s
+# yas = int(input("Yaşınızı giriniz: "))
+# print("Ehliyet alabilir mi?", yas >= 18 ) 
+
+# sayi = int(input("Bir sayı gir:"))
+# print("Sayı çift mi?", sayi % 2 == 0 )
+
+
+# tutar = float(input("Toplam tutar: "))
+# print("Bedava kargo kazandınız mı?: ", tutar > 250 )
+
+# derece = float(input("Hava kaç derece?"))
+# print("Hava donuyor mu?", derece <= 0 )
+
+# kilo = float(input("Kilo (kg): "))
+# boy = float(input("Boy(metre): "))
+# bmi = kilo /(boy**2)
+# print("Kritik seviyede mi?", bmi>30)
+
+# tahmin = int(input("1-10 arası bir sayı tuttum. Sence kaç?"))
+# print("Bildin mi? ", tahmin == 7)
+
+# fiyat = float(input("Ürünün fiyatı: "))
+# cüzdan = float(input("Cüzdanındaki para: "))
+# print("Satın alabilir misiniz:? ", cüzdan >= fiyat )
+
+# sayi = float(input("Sayı: "))
+# print("Sayı pozitif mi?: ", sayi >0 )
+
+# yas = int(input("Yaş: "))
+# print("Emekli olabilir mi? (65+ ) ", yas >= 65 )
+
+# hiz = int(input("hızınız: "))
+# print("Ceza yediniz mi? ", hiz > 120 )
+
+# oda = float(input("Oda sıcaklığı? : "))
+# print("Sıcaklık ideal midir? ", 18 <= oda <= 24)
+
+# can = int(input("Kalan can miktarını yaz: "))
+# print("Karakter yaşıyor mu?:", can>0 )
+
+# pelerin = input("Pelerin takılı mı? (evet/hayır:)")
+# print("Uçabilir misin? ", pelerin == "evet")
+
+# sure = int(input("Kaç saniye fırçaladın: ? "))
+# print("Tertemiz dişler için 2 dakikadan fazla olmalıdır:  ", sure >  120)
+
+sayfa_sayisi = 15
+biten = int(input("Kaç sayfa yazdın? "))
+print("Oyun zamanı geldi mi? ", biten == sayfa_sayisi )
